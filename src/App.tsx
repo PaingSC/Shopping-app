1c4d457dcff629ae6a7126e7ee31b2d04f7a8490
PaingSC <paingsoechit.job@gmail.com>
2025-05-10 16:06:23 +0700

    Installing Shopping-app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Greeter from "./components/Greeter";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Greeter />
+        <Greeter />
+        <Greeter />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Greeter.tsx
@@ -0,0 +1,7 @@
+import React, { type JSX } from "react";
+
+function Greeter(): JSX.Element {
+  return <h1>Hello!</h1>;
+}
+
+export default Greeter;
